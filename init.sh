c4a3c1abe4f752979719ff5a8ad1e84620433cd5
Nick Clifford <nick@nickclifford.com>
2026-02-11 16:37:07 +1300

    Improved init script

--- init.sh
+++ init.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
 echo "initialising worktree..."
-cd frontend
-echo "getting flutter packages..."
+
+# Get the directory of the script
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd "$SCRIPT_DIR"
+
+pub_directories=(
+  "claude_dart_sdk"
+  "codex_dart_sdk"
+  "agent_dart_sdk"
+  "frontend"
+)
+
+for dir in "${pub_directories[@]}"; do
+echo "Running flutter pub get in $dir..."
+cd "$SCRIPT_DIR/$dir"
 flutter pub get
-cd ..
+done
+
+cd $SCRIPT_DIR
 echo "initialisation complete."
